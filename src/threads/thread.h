7cebac093c2de3ba755676fd3c15a6561609bd7c
Masahisa Sekita <135545705+MasahisaSekita@users.noreply.github.com>
2025-05-28 14:07:27 -0400

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -153,6 +153,6 @@ struct donation {
 };
 
 bool comp_prior(struct list_elem *a, struct list_elem *b, void *aux);
-void ready_lst_sort(void);
+//void ready_lst_sort(void);
 
 #endif /* threads/thread.h */
